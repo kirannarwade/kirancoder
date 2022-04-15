25b4cd61e48d3513f15f0098239b2089b3db00f6
kirannarwade <90048456+kirannarwade@users.noreply.github.com>
2022-04-15 11:27:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects/")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/projects/ecommerce")
+def ecommerce():
+    return render_template("ecommerce.html")
+
+@app.route("/projects/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/projects/multi-todo")
+def multi():
+    return render_template("multi.html")
+
+@app.route("/projects/todo")
+def todo():
+    return render_template("todo.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
